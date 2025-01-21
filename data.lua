4238d3b40ddfdb5d60d158a3cccccf4f85dcfe79
Brad Hansen <brad.m.hansen@gmail.com>
2025-01-21 18:34:36 +1100

    Add new fluid and recipe using spoilage

---
+++ data.lua
@@ -0,0 +1,59 @@
+local fluid = {
+    type = "fluid",
+    name = "nutrient-solution",
+    icon = "__fluid-nutrients__/gfx/nutrient-solution.png",
+    subgroup = "fluid",
+    order = "b[new-fluid]-d[gleba]-a[nutrient-solution]",
+    default_temperature = 20,
+    max_temperature = 100,
+    heat_capacity = "2kJ",
+    base_color = {0.6, 0.6, 0.9},
+    flow_color = {0.7, 0.7, 0.95},
+    auto_barrel = false
+}
+
+local recipe = {
+    type = "recipe",
+    name = "nutrient-solution-from-spoilage",
+    icon = "__space-age__/graphics/icons/nutrients-from-spoilage.png",
+    category = "organic-or-chemistry",
+    subgroup = "agriculture-processes",
+    order = "c[nutrient-solution]-a[nutrient-solution-from-spoilage]",
+    enabled = false,
+    allow_productivity = true,
+    energy_required = 2,
+    ingredients = {{
+        type = "item",
+        name = "spoilage",
+        amount = 10
+    }, {
+        type = "fluid",
+        name = "water",
+        amount = 10
+    }},
+    results = {{
+        type = "fluid",
+        name = "nutrient-solution",
+        amount = 10
+    }},
+    crafting_machine_tint = {
+        primary = {
+            r = 0.8,
+            g = 0.9,
+            b = 1,
+            a = 1.000
+        },
+        secondary = {
+            r = 0.5,
+            g = 0.5,
+            b = 0.8,
+            a = 1.000
+        }
+    }
+}
+
+data:extend{fluid, recipe}
+table.insert(data.raw.technology["agriculture"].effects, {
+    type = "unlock-recipe",
+    recipe = "nutrient-solution-from-spoilage"
+})
