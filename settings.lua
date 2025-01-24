15fd60f4adff45f4d70a5aa4117679dc2c1816c8
Brad Hansen <brad.m.hansen@gmail.com>
2025-01-25 00:58:20 +1100

    fix mod settings ordering

--- settings.lua
+++ settings.lua
@@ -11,20 +11,20 @@ data:extend({
         name = "captive-biter-spawner-use-nutrient-solution",
         setting_type = "startup",
         default_value = true,
-        order = "a",
+        order = "b",
     },
     {
         type = "bool-setting",
         name = "fish-breeding-net-positive-nutrient-solution",
         setting_type = "startup",
         default_value = true,
-        order = "a",
+        order = "c",
     },
     {
         type = "bool-setting",
         name = "biochamber-nutrient-solution-flow-through",
         setting_type = "startup",
         default_value = false,
-        order = "a",
+        order = "d",
     }
 })
